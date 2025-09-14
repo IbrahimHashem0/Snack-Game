d48366b4b18814361074feaa358b06e72019a033
Ibrahim Hashem Mustafa <161343838+IbrahimHashem0@users.noreply.github.com>
2025-09-14 08:08:34 +0300

    Update

---
+++ Snack Game/src/snakgame/GameFrame.java
@@ -0,0 +1,15 @@
+package snakegame;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    public GameFrame() {
+        this.add(new GamePanel());
+        this.setTitle("Snake Game - OOP + DSA");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
